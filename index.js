1e79ad821fd8370ea55418c9d6ea6e58991385f0
unknown <theamirdv@gmail.com>
2020-11-16 12:03:04 +0330

    asasas

--- index.js
+++ index.js
@@ -45,10 +45,9 @@ client.once('ready', () => {
 
 
 
-client.on('message', async message => {
+client.on('message', async (message, prefix) => {
         
-
-    
+    const MainPrefix = prefix
     const maximum = 16777215
     const minimum = 0
     const RandomNumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
@@ -177,6 +176,7 @@ await mongo().then(async (mongoose) => {
                             .setAuthor(`[ ${song.durationhours} : ${song.durationminute} : ${song.durationsecond} ] - ${song.title}`, 'https://cdn.discordapp.com/attachments/727509077441380433/773553428529414184/download.jpg', `${song.url}`)
                             .setColor(RandomNumber)
                             .setImage(Thumbnail)
+                            .setFooter(`Music Queue : ${queueConstruct.songs.length} | Volume : ${queueConstruct.volume} | Prefix : ${MainPrefix}`)
                         await EmbedMessage.edit(`​​                                                                                                                                                        
 __**QUEUE LIST:**__ \n ${queueConstruct.songs.map(song => `**-** ${song.title}`).join('\n')}`, StartedPlaying)
                             try {
@@ -232,6 +232,7 @@ __**QUEUE LIST:**__ \n ${queueConstruct.songs.map(song => `**-** ${song.title}`)
                         .setAuthor(`[ ${Music.durationhours} : ${Music.durationminute} : ${Music.durationsecond} ] ${Music.title}`, 'https://cdn.discordapp.com/attachments/727509077441380433/773553428529414184/download.jpg', `${Music.url}`)
                         .setImage(Thumbnail)
                         .setColor(RandomNumber)
+                        .setFooter(`Music Queue : ${serverQueue.songs.length} | Volume : ${serverQueue.volume} | Prefix : ${MainPrefix}`)
                     await EmbedMessage.edit(`​​                                                                                                                                                        
 __**QUEUE LIST:**__ \n ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}`, StartedPlaying)
                         try {
